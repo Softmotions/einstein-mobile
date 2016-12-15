b3d5fe8fa09a40aa3fd6b28665e7de59aa190a6b
Vyacheslav Tyutyunkov <tve@softmotions.com>
2016-12-15 18:06:56 +0700

    - small items

--- src/einstein.js
+++ src/einstein.js
@@ -16,17 +16,52 @@ for (let i = 0; i < 6; ++i) {
 // todo: calculate from dimension & ratio
 const styleCfg = {
   space: 2,
-  item: 35,
+  item: 16,
+  group: 50,
   border: 1,
 };
 
+const state = {
+  field: [[0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0]]
+};
+
 export class GameField extends Component {
+
+  renderItem(i, j, k) {
+    const key = 'item_' + i + '_' + j + '_' + k;
+    const src = 'item' + (i + 1) + (k + 1);
+    return (
+      <View key={key} style={styles.itemBox}>
+        { ((j - i % 2 + 1) / 2 + i + k) % 2 == 0 ? <Image style={styles.item} source={{uri: src}}/> : null }
+      </View>
+    );
+  };
+
+  renderGroupItemsLine(i, j, n) {
+    return (
+      <View style={styles.groupItemsRow}>
+        {items.filter((t) => {
+          return t >= n * (size / 2) && t < (n + 1) * (size / 2)
+        }).map((k) => {
+          return this.renderItem(i, j, k)
+        })}
+      </View>
+    );
+  }
 
   renderGroupItem(i, j) {
     const key = 'group_' + i + '_' + j;
     const src = 'item' + (i + 1 ) + (j + 1);
     return (
-      <Image key={key} style={styles.groupItem} source={{uri : src}}/>
+      <View key={key}>
+        {state.field[i][j] ?
+          <View style={styles.groupItem}>
+            {this.renderGroupItemsLine(i, j, 0)}
+            {this.renderGroupItemsLine(i, j, 1)}
+          </View> :
+          <Image style={styles.groupItem} source={{uri : src}}/>
+        }
+      </View>
     );
   }
 
@@ -53,6 +88,7 @@ export class GameField extends Component {
 }
 
 export default class Einstein extends Component {
+
   render() {
     return (
       <GameField/>
@@ -62,8 +98,8 @@ export default class Einstein extends Component {
 
 const styles = StyleSheet.create({
   gamefield: {
-    height: styleCfg.item * size + styleCfg.space * (size + 1),
-    width: styleCfg.item * size + styleCfg.space * (size + 1) + styleCfg.border * 2,
+    height: styleCfg.group * size + styleCfg.space * (size + 1),
+    width: styleCfg.group * size + styleCfg.space * (size + 1) + styleCfg.border * 2,
     borderWidth: styleCfg.border,
     borderColor: '#000',
     flexDirection: 'column',
@@ -72,19 +108,36 @@ const styles = StyleSheet.create({
   },
 
   row: {
-    height: styleCfg.item,
-    width: styleCfg.item * size + styleCfg.space * (size - 1),
+    height: styleCfg.group,
+    width: styleCfg.group * size + styleCfg.space * (size - 1),
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 0
   },
 
   groupItem: {
+    height: styleCfg.group,
+    width: styleCfg.group,
+    borderWidth: styleCfg.border,
+    borderColor: '#000',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  groupItemsRow: {
+    flexDirection: 'row',
+  },
+
+  itemBox: {
+    height: styleCfg.item,
+    width: styleCfg.item,
+  },
+
+  item: {
     height: styleCfg.item,
     width: styleCfg.item,
     borderWidth: styleCfg.border,
     borderColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+  },
 });
