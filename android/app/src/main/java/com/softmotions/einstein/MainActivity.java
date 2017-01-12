b5ba7bf534d2128cefa74dd0b9a5a4f5bbc335b6
Vyacheslav Tyutyunkov <tve@softmotions.com>
2017-01-12 17:22:06 +0700

    - google play games integration

--- android/app/src/main/java/com/softmotions/einstein/MainActivity.java
+++ android/app/src/main/java/com/softmotions/einstein/MainActivity.java
@@ -1,12 +1,19 @@
 package com.softmotions.einstein;
 
 import android.os.Bundle;
-import android.view.WindowManager;
-import com.facebook.react.ReactActivity;
-
+import android.util.Log;
 import com.cboy.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactFragmentActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.games.Games;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactFragmentActivity
+        implements OnConnectionFailedListener, ConnectionCallbacks {
+
+    private GoogleApiClient mGoogleApiClient;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -19,7 +26,30 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
         super.onCreate(savedInstanceState);
+        SplashScreen.show(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //todo fail resolve
+        Log.e("\n\n", "failed");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.e("\n\n", "connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("\n\n", "suspended");
     }
 }
