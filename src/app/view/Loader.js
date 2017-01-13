7de13c2d415721fa967bf541c696d288e8601e83
Vyacheslav Tyutyunkov <tve@softmotions.com>
2017-01-13 18:14:45 +0700

    - loader

--- src/app/view/Loader.js
+++ src/app/view/Loader.js
@@ -3,45 +3,19 @@
 import React, {Component} from 'react';
 import {
   View,
-  Image,
-  Animated,
-  Easing
+  Image
 } from 'react-native';
 
 class Loader extends Component {
   constructor(props) {
     super(props);
-    this._rotate = new Animated.Value(0);
   }
 
-  rotation = () => {
-    this._rotate.setValue(0);
-    Animated.timing(this._rotate, {
-      toValue: 360,
-      duration: 1000,
-      delay: 0,
-      easing: Easing.linear
-    }).start(() => this.rotation());
-  };
-
-  componentDidMount() {
-    this.rotation();
-  }
-
-  render() {
-    let ri = this._rotate.interpolate({
-      inputRange: [0, 360],
-      outputRange: ['0deg', '360deg']
-    });
-
-    return (
-      <View>
-        <Animated.View style={{transform: [{rotate: ri}]}}>
-          <Image source={require('../../../images/logo_plain.png')} style={{width: 36, height: 36}}/>
-        </Animated.View>
-      </View>
-    )
-  }
+  render = () => (
+    <View>
+      <Image source={require('../../../images/loader.gif')} style={{width: 36, height: 36}}/>
+    </View>
+  )
 }
 
 export {
